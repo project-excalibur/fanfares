360dc8dbfcd79baf9a6eeb5e473579cf43581204
arkin0x <arkinox@pm.me>
2024-06-06 16:17:44 -0500

    center top logo

--- apps/fanfares/src/app/components/MobileTopNavbar.tsx
+++ apps/fanfares/src/app/components/MobileTopNavbar.tsx
@@ -35,8 +35,13 @@ function MobileTopNavbar(props: MobileTopNavbarProps) {
     })
   }
   return (
+<<<<<<< HEAD
     <div className="grid-cols-3 grid  items-center justify-between md:hidden py-3 px-4 w-full drop-shadow-md border-b-2 border-buttonAccentHover backdrop-blur-md">
       {/* <Link href="/" className="w-7">
+=======
+    <div className="flex items-center justify-between md:hidden py-3 px-4 w-full drop-shadow-md border-b-2 border-buttonAccentHover backdrop-blur-md">
+      <Link href="/" className="w-7 mr-auto">
+>>>>>>> aaf946b (center top logo)
         <FontAwesomeIcon icon={faHomeAlt} className="text-white" />
       </Link> */}
       <DropdownMenuMobile />
